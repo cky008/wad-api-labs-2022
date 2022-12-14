80cb0a6e8fac97d8eddb0b502c2758e106044a83
Kaiyu Chen <greatcky83@gmail.com>
2022-12-14 18:01:04 +0000

    Topic 11 - Passport

--- movies-api/index.js
+++ movies-api/index.js
@@ -6,7 +6,8 @@ import './db';
 import './seedData'
 import usersRouter from './api/users';
 import session from 'express-session';
-import authenticate from './authenticate';
+import passport from './authenticate';
+
 
 dotenv.config();
 
@@ -25,13 +26,9 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.use(session({
-  secret: 'ilikecake',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(passport.initialize());
 
-app.use('/api/movies', authenticate, moviesRouter);
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
 
 app.use('/api/genres', genresRouter);
 
